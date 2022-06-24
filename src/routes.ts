e3761dc8d3afb360e07108ece29bb209c3921d5c
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2022-06-24 16:46:10 -0300

    Configurando estrutura

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,8 @@
 import {Router, Request, Response} from 'express'
+import { CreateUserController } from './controllers/user/CreateUserController';
 
 const router = Router();
 
-router.get('/teste', (req: Request, res: Response) => {
-  return res.json({ app: "sujeito pizza" })
-})
+router.post('/users', new CreateUserController().handle)
 
 export { router };
