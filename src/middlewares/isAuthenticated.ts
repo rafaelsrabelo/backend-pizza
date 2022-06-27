9cac26201e8bc1489d10e6662f40e6f1efd64e59
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2022-06-27 10:55:02 -0300

    adicionando middleware

---
+++ src/middlewares/isAuthenticated.ts
@@ -0,0 +1,10 @@
+import {NextFunction, Request, Response} from 'express'
+
+export function isAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction
+  ) {
+    console.log("CHAMOU ESSE MIDLEWARE")
+    return next();
+}
