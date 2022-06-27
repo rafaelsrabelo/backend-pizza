1deb75090adabe297a9a7eb356f25151342c0117
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2022-06-27 14:24:17 -0300

    criando rota para produto

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController'
 import { DetailUserController } from './controllers/user/DetailUserController';
 import { ListCategoryController } from './controllers/category/ListCategoryController';
+import { CreateProductController } from './controllers/product/CreateProductController';
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 import { isAuthenticated } from './middlewares/isAuthenticated';
 const router = Router();
@@ -14,5 +15,8 @@ router.get('/me', isAuthenticated, new DetailUserController().handle)
 // rota de categoria
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 router.get('/category', isAuthenticated, new ListCategoryController().handle)
+
+// rotas de produtos
+router.post('/product', isAuthenticated, new CreateProductController().handle)
 
 export { router };
