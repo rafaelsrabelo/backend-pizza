cd04765be362dc6a1b058fff46196f646f214e68
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2022-06-30 01:37:13 -0300

    funcionalidade para concluir o pedido

--- src/routes.ts
+++ src/routes.ts
@@ -16,6 +16,7 @@ import { DetailOrderController } from './controllers/order/DetailOrderController
 import { isAuthenticated } from './middlewares/isAuthenticated';
 import { RemoveItemController } from './controllers/order/RemoveItemController';
 import uploadConfig from './config/multer';
+import { FinishOrderController } from './controllers/order/FinishOrderController';
 
 const router = Router();
 
@@ -45,5 +46,7 @@ router.put('/order/update', isAuthenticated, new SendOrderController().handle)
 router.get('/orders', isAuthenticated, new ListOrdersController().handle)
 
 router.get('/order/detail', isAuthenticated, new DetailOrderController().handle)
+
+router.put('/order/finish', isAuthenticated, new FinishOrderController().handle)
 
 export { router };
